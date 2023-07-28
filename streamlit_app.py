bbb9be5f41eedd6f20c6682e4a3300eb2c1ab272
mikesasing4 <140698049+mikesasing4@users.noreply.github.com>
2023-07-28 17:17:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector
+
 requirements.txt
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
